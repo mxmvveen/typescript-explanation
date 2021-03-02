996feb54f9c0f4d49f7b7ee37b0189a0a875e167
mxmvveen <mxmvveen@gmail.com>
2021-03-02 14:01:51 +0100

    changed

--- index.js
+++
@@ -1,7 +0,0 @@
-var names = null > , _a = 'John';
-null, 'Jane', 'Walter', null;
-;
-var nameList = names.filter(function (name) {
-    if (name === void 0) { name = null; }
-    return name !== null;
-});
--- index.ts
+++
@@ -1,4 +0,0 @@
-const names: Array<string | null> = ['John', null, 'Jane', 'Walter', null];
-
-const nameList: string[] = names.filter((name: string | null) => name !== null) as string[];
-
